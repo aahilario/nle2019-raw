b1b60dc82ff012e553fc1c4d9cfd119757ff0d77
avahilario@gmail.com <avahilario@gmail.com>
2019-05-30 15:34:38 +0800

    Newly-fetched batch of 51 contest data records

--- push.sh
+++ push.sh
@@ -3,9 +3,13 @@ N=50
 while true; do
   NF=$(git status | grep 'new file:' | wc -l)
   if [ $NF -gt $N ]; then 
-    git commit -a -m "Newly-fetched batch of $NF contest data records" && git push 
+    git commit -a -m "Newly-fetched batch of $NF contest data records" && git push && /bin/echo -n '*' && sync
   else
+    sync
+    /bin/echo -n '\'
     sleep 10
+    /bin/echo -n '/'
+    sync
   fi
   /bin/echo -n '.'
   ./tool.sh
